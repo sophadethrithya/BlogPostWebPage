716a7e221882fea1a9576221f24715c592741c03
sophadeth rithya <sophadethrithya@gmail.com>
2020-02-15 14:15:12 -0800

    change port

--- app.js
+++ app.js
@@ -81,6 +81,6 @@ Post.findOne({_id:requestedID},function(err,post){
 
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
